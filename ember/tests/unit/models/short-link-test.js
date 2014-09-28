77991fa4f280f27b00c9c67bcfb33b2b80cf0c6e
Ryan Recht <ryanere@gmail.com>
2014-09-28 12:18:28 -0700

    app has shorten-test.js renamed

--- ember/tests/unit/models/short-link-test.js
+++ ember/tests/unit/models/short-link-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForModel('short-link', 'Shorten', {
+moduleForModel('short-link', 'ShortLink', {
   // Specify the other units that are required for this test.
   needs: []
 });
