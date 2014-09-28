3ee6a70f90f33fbea8f9ae41aaaca0c302747d6c
Ryan Recht <ryanere@gmail.com>
2014-09-28 09:42:19 -0700

    app has active_model_serializer, rack_cors and rspec

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -15,6 +15,13 @@ Bundler.require(*Rails.groups)
 
 module Shorty
   class Application < Rails::Application
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins 'http://localhost:4200'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
