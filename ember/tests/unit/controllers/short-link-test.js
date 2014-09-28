8a4475c106fdc711ccf4f9290c2aa85389a404a2
Ryan Recht <ryanere@gmail.com>
2014-09-28 13:47:14 -0700

    short-link-controller -> short-link

--- ember/tests/unit/controllers/short-link-test.js
+++ ember/tests/unit/controllers/short-link-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:short-link-controller', 'ShortLinkControllerController', {
+moduleFor('controller:short-link', 'ShortLinkController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
