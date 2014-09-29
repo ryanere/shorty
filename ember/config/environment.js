4f1b5e1e85e75946fd97329f9b8d3de13fada339
Ryan Recht <ryanere@gmail.com>
2014-09-28 21:16:17 -0700

    ember router uses hashbang

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -5,7 +5,8 @@ module.exports = function(environment) {
     modulePrefix: 'shorty',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
