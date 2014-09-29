4e814f5ad450ebbee0c2183f9e0d5108612e2f03
Ryan Recht <ryanere@gmail.com>
2014-09-28 21:21:54 -0700

    remove trailing slash from environment config

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.HOST = 'http://shortyrules.herokuapp.com/';
+    ENV.APP.HOST = 'http://shortyrules.herokuapp.com';
   }
 
   return ENV;
